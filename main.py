a0d3149b52c3f0e8f5e8c933dade519334e6ec32
David Cereijo <davidcereijo@live.com>
2017-11-27 20:49:50 +0100

    ejemplo de uso del nuevo manejo de carga de html bajo demanda

--- main.py
+++ main.py
@@ -6,4 +6,9 @@ logging.config.dictConfig(yaml.load(open('conf/logging.yaml')))
 tfmktparser.source = tfmktparser.LOCAL
 season = tfmktparser.Season()
 season.init_clubs()
-season.init_players()
+season['fc-barcelona'].create_soup()
+season['fc-barcelona'].init_players()
+season['fc-barcelona']['lionel-messi'].create_soup()
+season['fc-barcelona']['lionel-messi'].init_tables()
+
+#season['fc-barcelona']['lionel-messi'].parse_tables()
